a32c7bad731ba351fa2895b93b82f1e9e900171b
Matt Clarke-Lauer <mdlauer@gmail.com>
2023-08-20 06:45:12 -0700

    add initial screen creation

--- src/game/game.py
+++ src/game/game.py
@@ -18,29 +18,49 @@ class Context():
 class Game():
     def __init__(self, max_x, max_y):
         self.context = Context(max_x, max_y)
+        self.screen = None
 
     def run_game(self):
         # pygame init
+
+        pygame.init()
         self.running = True
 
+        # Set the size of the display screen
+        screen_size = (self.context.max_x, self.context.max_y)
+
+        self.screen = pygame.display.set_mode(screen_size)
         while self.running:
             self._game_tick()
         # pygame exit
+        pygame.exit()
 
     def _game_tick(self):
         # runs once for every game tick
+
         self._process_input()
         self._update_state()
         self._draw_screen()
-        pass
 
     def _draw_screen(self):
         # redraw scene based on new input state
-        pass
+
+        # Clear the screen
+        self.screen.fill((0, 0, 0))
+
+        # Draw something on the screen
+        pygame.draw.circle(self.screen, (255, 0, 0), (320, 240), 100)
+
+        # Update the display
+        pygame.display.update()
 
     def _process_input(self):
         # case statement tracking input state
-        pass
+        # Check for events
+        for event in pygame.event.get():
+            # If the user quits, exit the loop
+            if event.type == pygame.QUIT:
+                break
 
     def _update_state(self):
         # take input state and update game state
